e340beea5ecb2f4db8363c3a1dc347bc2c5e49f5
Varun Ramnani <varunhhhrahul@gmail.com>
2021-06-06 16:14:55 +0530

    created constant messages

---
+++ constants/messages.js
@@ -0,0 +1,12 @@
+// common errors for login and registration form
+
+//Password Errors
+export const PASSWORD_REQUIRED = 'Password is required!';
+export const CONFIRM_PASSWORD_REQUIRED = 'Please confirm password to register!';
+export const PASSWORD_SHORT_ERROR = 'Password should be 6 characters minimum.';
+export const PASSWORDS_MATCH_ERROR = `Passwords don't match`;
+
+//Email Errors
+export const EMAIL_INVALID_ERROR = 'Invalid email address!';
+export const EMAIL_NOT_LONG_ERROR = 'Email not long enough!';
+export const EMAIL_REQUIRED = 'Email is required!';
